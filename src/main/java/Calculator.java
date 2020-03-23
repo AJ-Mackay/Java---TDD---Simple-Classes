24dbd4c4036f6c383a016ec89302edde60b5cb86
Ruiner85 <paul.m.mackay85@gmail.com>
2020-03-23 16:49:52 +0000

    finish Calculator tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,13 +2,29 @@ public class Calculator {
 
     private int number1;
     private int number2;
+    private double number3;
+    private double number4;
 
-    public Calculator(int number1, int number2){
+    public Calculator(int number1, int number2, double number3, double number4){
         this.number1 = number1;
         this.number2 = number2;
+        this.number3 = number3;
+        this.number4 = number4;
     }
 
-    public int add() {
+    public int add(){
         return this.number1 + this.number2;
+    }
+
+    public int subtract(){
+        return this.number1 - this.number2;
+    }
+
+    public int multiply(){
+        return this.number1 * this.number2;
+    }
+
+    public double divide(){
+        return this.number3 / this.number4;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,12 +8,27 @@ public class CalculatorTest {
 
     @Before
     public void before(){
-        calculator = new Calculator(8,5);
+        calculator = new Calculator(8,5, 17.5, 5.0);
     }
 
     @Test
     public void canAdd(){
         assertEquals(13, calculator.add());
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(3, calculator.subtract());
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(40, calculator.multiply());
+    }
+
+    @Test
+    public void canDivide(){
+        assertEquals(3.5, calculator.divide(), 0.01);
     }
 }
 
